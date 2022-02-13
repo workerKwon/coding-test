1624219d4702f72f0e85c61f6fe28a31dfb10de4
workerKwon <dmdmdm1322@gmail.com>
2022-02-13 15:44:28 +0900

    Fix 간단한 린터 만들어서 스택 사용해보기

--- stackLinter.js
+++ stackLinter.js
@@ -14,7 +14,6 @@ class Linter {
         } else if (this.closing_brace(char)) {
           if (this.closes_most_recent_opening_brace(char)) {
             this._stack.pop()
-            console.log("stack", this._stack)
           } else {
             this._error = `Incorrect closing brace: ${char} at index ${index}`;
             return
@@ -22,8 +21,8 @@ class Linter {
         }
       });
   
-      if (this._stack.any) {
-        this._error = `${this._stack.last} does not have a closing brace`
+      if (this._stack.length) {
+        this._error = `${this._stack[this._stack.length - 1]} does not have a closing brace`
       }
     };
   
@@ -44,11 +43,10 @@ class Linter {
     };
   
     closes_most_recent_opening_brace(char) {
-        console.log(this.opening_brace_of(char) == this.most_recent_opening_brace())
-      return this.opening_brace_of(char) == this.most_recent_opening_brace
-    }
+      return this.opening_brace_of(char) == this.most_recent_opening_brace()
+    };
   };
   
   let linter = new Linter;
-  linter.lint("( var x = { y: [1, 2, 3] } )");
+  linter.lint("( var x = { y: [1, 2, 3] }");
   console.log(linter.error)
