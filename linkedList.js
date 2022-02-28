9d984a9cca43075fb05f07c3d7c6a62ad9a94139
workerKwon <dmdmdm1322@gmail.com>
2022-02-28 20:30:25 +0900

    implement LinkedList

---
+++ linkedList.js
@@ -0,0 +1,45 @@
+class Node {
+    get data() {
+        return this._data
+    }
+
+    set data(data) {
+        this._data = data
+    }
+
+    get next_node() {
+        return this._next_node
+    }
+
+    set next_node(next_node) {
+        this._next_node = next_node
+    }
+
+    constructor(data) {
+        this._data = data
+    }
+}
+
+class LinkedList {
+    get first_node() {
+        return this._first_node
+    }
+
+    set first_node(first_node) {
+        this._first_node = first_node
+    }
+
+    constructor(first_node) {
+        this._first_node = first_node
+    }
+}
+
+const node1 = new Node("one")
+const node2 = new Node("two")
+node1._next_node = node2
+const node3 = new Node("three")
+node2._next_node = node3
+const node4 = new Node('four')
+node3._next_node = node4
+
+const list = new LinkedList(node1)
