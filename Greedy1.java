b3bb6a84a3cb7e5bcbf498ddc938ae319f85e402
workerKwon <dmdmdm1322@gmail.com>
2022-03-20 13:42:02 +0900

    큰 수의 법칙 두번째 풀이 - 반복되는 패턴을 수학적으로 활용

--- Greedy1.java
+++ Greedy1.java
@@ -14,14 +14,48 @@ import javax.swing.SortOrder;
 public class Greedy1 {
 
     public static void main(String[] args) {
+        firstMethod();
+        secondMethod();
+    }
+
+    private static void secondMethod() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int first = arr[n-1];
+        int second = arr[n-2];
+
+        /**
+         * 반복되는 패턴을 수학적으로 이용
+         * 총 더해야하는 횟수 / 반복되는 패턴 = 패턴이 반복되는 횟수
+         * 패턴이 반복되는 횟수 * 하나의 인덱스를 반복할 수 있는 최대 횟수 = 가장 큰수를 더해야하는 횟수
+         *     + 패턴이 반복되는 횟수가 나누어 떨어지지 않을 때는 나머지만큼 가장 큰수를 더해줘야한다.
+         */
+        int cnt = (m / (k + 1) * k) + (m % (k + 1));
+
+        int result = 0;
+        result += first * cnt;
+        result += (m - cnt) * second;
+
+        System.out.println(result);
+    }
+
+    private static void firstMethod() {
         Scanner scanner = new Scanner(System.in);
 
-        int n;
-        n = scanner.nextInt();
-        int m;
-        m = scanner.nextInt();
-        int k;
-        k = scanner.nextInt();
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int k = scanner.nextInt();
 
         int[] data = new int[n];
         for(int i = 0; i < data.length; i++) {
@@ -52,3 +86,4 @@ public class Greedy1 {
         System.out.println(result);
     }
 }
+
