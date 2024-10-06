a6bf09434eea8612987f9f5e6d9fe236d3921768
workerKwon <dmdmdm1322@gmail.com>
2024-10-06 13:18:47 +0900

    dfs, bfs

--- 미로탈출.java
+++ 미로탈출.java
@@ -1,58 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class 미로탈출 {
 
-    private static Integer N;
-    private static Integer M;
-    private static int[][] GRAPH;
+    static int N;
+    static int M;
+    static int[][] GRAPH;
 
-    private static final int[] dx = {-1, 0, 1, 0};
-    private static final int[] dy = {0, -1, 0, 1};
-
-    public static void main(String[] args) throws IOException {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] firstRow = br.readLine().split(" ");
-        N = Integer.parseInt(firstRow[0]);
-        M = Integer.parseInt(firstRow[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         GRAPH = new int[N][M];
 
-        for (int i = 0; i < N; i++) {
-            String[] st = br.readLine().split("");
-            for (int j = 0; j < M; j++) {
-                GRAPH[i][j] = Integer.parseInt(st[j]);
+        for(int i = 0; i < N; i++) {
+            String row = br.readLine();
+            for(int j = 0; j < M; j++) {
+                GRAPH[i][j] = row.charAt(j) - '0';
             }
         }
 
-        System.out.println(bfs(0, 0));
-    }
-
-    private static int bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-
-        while (!queue.isEmpty()) {
-            int[] xy = queue.poll();
-            int n = xy[0];
-            int m = xy[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = n + dx[i];
-                int ny = m + dy[i];
-
-                if (nx <= -1 || nx >= N || ny <= -1 || ny >= M) continue;
-                if (GRAPH[nx][ny] == 0) continue;
-                if (GRAPH[nx][ny] == 1) {
-                    GRAPH[nx][ny] = GRAPH[n][m] + 1;
-                    queue.add(new int[]{nx, ny});
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0 ,0});
+        while (q.size() > 0) {
+            int[] xy = q.poll();
+            int x = xy[0];
+            int y = xy[1];
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M || GRAPH[nx][ny] == 0) continue;
+                if(GRAPH[nx][ny] == 1) {
+                    GRAPH[nx][ny] = GRAPH[x][y] + 1;
+                    q.offer(new int[]{nx, ny});
                 }
             }
         }
-        return GRAPH[N - 1][M - 1];
+
+        System.out.println(GRAPH[N-1][M-1]);
     }
 }
--- 음료수얼려먹기.java
+++ 음료수얼려먹기.java
@@ -1,53 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
+/*
+ * N * M 크기의 얼음 틀이 있다. 구멍이 뚫린 부분은 0, 칸막이가 존재하는 부분은 1
+ * 구멍이 뚫린 부분끼리 상,하,좌,우로 붙어 있는 경우 서로 연결되어 있는 것으로 간주한다.
+ * 이 때 만들 수 있는 얼음의 개수를 구하는 프로그램 작성
+ */
 public class 음료수얼려먹기 {
-    static Integer N;
-    static Integer M;
-
-    static int[][] GRAPH;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        String[] firstRow = br.readLine().split(" ");
-        N = Integer.parseInt(firstRow[0]);
-        M = Integer.parseInt(firstRow[1]);
+        int[][] A = new int[N][M];
+        for(int i = 0 ; i < N; i++) {
+            String row = br.readLine();
+            for(int j = 0; j < M; j++) {
+                A[i][j] = row.charAt(j) - '0';
+            }
+        }
 
-        GRAPH = new int[N][M];
-
+        int cnt = 0;
         for(int i = 0; i < N; i++) {
-            String[] st = br.readLine().split("");
-            for (int j = 0; j < M; j++) {
-                GRAPH[i][j] = Integer.parseInt(st[j]);
+            for(int j = 0; j < M; j++) {
+                if(dfs(i, j, A, N, M) == true) cnt++;
             }
         }
 
-        int count = 0;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (dfs(i, j)) count++;
-            }
-        }
-
-        System.out.println(count);
+        System.out.println(cnt);
     }
 
-    static boolean dfs(int x, int y) {
-        if (x <= -1 || x >= N || y <= -1 || y >= M) {
-            return false;
-        }
-
-        if (GRAPH[x][y] == 0) {
-            GRAPH[x][y] = 1;
-            dfs(x - 1, y);
-            dfs(x + 1, y);
-            dfs(x, y - 1);
-            dfs(x, y + 1);
+    static boolean dfs(int x, int y, int[][] A, int xMax, int yMax) {
+        if(x <= -1 || x >= xMax || y <= -1 || y >= yMax) return false;
+        if(A[x][y] == 0) {
+            A[x][y] = 1;
+            dfs(x-1, y, A, xMax, yMax);
+            dfs(x, y-1, A, xMax, yMax);
+            dfs(x+1, y, A, xMax, yMax);
+            dfs(x, y+1, A, xMax, yMax);
             return true;
         }
-
         return false;
     }
 }
